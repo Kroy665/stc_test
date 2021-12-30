976ffd3e80a9e2bdda2a184e559fe823904fae4a
Kroy665 <kroy665@gmail.com>
2021-12-30 14:40:31 +0530

    fatch add

--- src/App.js
+++ src/App.js
@@ -20,28 +20,42 @@ function App() {
         const name = event.target.name;
         const value = event.target.value;
         setPostData((values) => ({ ...values, [name]: value }));
-        
     };
 
     const onFormSubmit = (e) => {
         e.preventDefault();
-        console.log(postData)
-
+        console.log(postData);
 
         const headers = {
-          'Content-Type': 'application/json',
-          'X-ClientCode' : '61238669970',
-          'Access-Control-Allow-Origin': '*',
-        }
-        const sendData = async() =>{
-            const data = await axios.post(createOTPurl,{
-              "DirectPaymentAuthorizeV4RequestMessage": postData
-            },{
-              headers: headers
-            })
-            console.log(data)
-        }
-        sendData()
+            "Content-Type": "application/json",
+            "X-ClientCode": "61238669970",
+        };
+        // const sendData = async () => {
+        //     const data = await axios.get(
+        //         createOTPurl,
+        //         {
+        //             DirectPaymentAuthorizeV4RequestMessage: postData,
+        //         },
+        //         {
+        //             mode: "no-cors",
+        //             headers: headers,
+        //         }
+        //     );
+        //     console.log(data);
+        // };
+        // sendData();
+
+        const requestOptions = {
+            method: "POST",
+            mode: "no-cors",
+            headers: headers,
+            body: {
+                DirectPaymentAuthorizeV4RequestMessage: postData,
+            },
+        };
+        fetch(createOTPurl, requestOptions)
+            .then((response) => console.log(response))
+            .then((data) => console.log(data));
     };
 
     return (
