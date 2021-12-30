74cabffb1ef2053175cb1dd7cc8684185e445088
Kroy665 <kroy665@gmail.com>
2021-12-30 14:01:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const createOTPurl =
+        "https://Test.B2B.stcpay.com.sa/B2B.DirectPayment.WebApi/DirectPayment/V4/DirectPaymentAuthorize";
+    const [postData, setPostData] = useState({
+        BranchID: "string",
+        TellerID: "string",
+        DeviceID: "string",
+        RefNum: "123457",
+        BillNumber: "333",
+        MobileNo: "966539342897",
+        Amount: "100",
+        MerchantNote: "string",
+    });
+    const onDataChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setPostData((values) => ({ ...values, [name]: value }));
+        
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(postData)
+
+
+        const headers = {
+          'Content-Type': 'application/json',
+          'X-ClientCode' : '61238669970',
+          'Access-Control-Allow-Origin': '*',
+        }
+        const sendData = async() =>{
+            const data = await axios.post(createOTPurl,{
+              "DirectPaymentAuthorizeV4RequestMessage": postData
+            },{
+              headers: headers
+            })
+            console.log(data)
+        }
+        sendData()
+    };
+
+    return (
+        <div className="App">
+            <form onSubmit={onFormSubmit}>
+                <label htmlFor="Amount">Amount</label>
+                <input
+                    type="text"
+                    name="Amount"
+                    value={postData.Amount}
+                    onChange={onDataChange}
+                />
+                <label htmlFor="MobileNo">Mobile No</label>
+                <input
+                    type="number"
+                    value={postData.MobileNo}
+                    name="MobileNo"
+                    onChange={onDataChange}
+                />
+                <input type="submit" />
+            </form>
+        </div>
+    );
 }
 
 export default App;
