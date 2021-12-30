2ebc254e1266e2458aa8e873656b1a4d10a5a215
Kroy665 <kroy665@gmail.com>
2021-12-30 14:45:47 +0530

    fatch add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import React, { useState } from "react";
 import "./App.css";
-import axios from "axios";
+// import axios from "axios";
 
 function App() {
     const createOTPurl =
